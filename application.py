e8efe2e3a05c3fdb891a5da241cac254313f3a8d
James L Barnett <james_barnett@fas.harvard.edu>
2018-07-11 16:50:15 +0000

    fixes things up, prettier and more correct location page far along with comments showing, now need to disallow users leaving more than one

--- application.py
+++ application.py
@@ -34,23 +34,25 @@ def login():
   password = request.form.get("password")
   newusername = request.form.get("newusername")
   newpassword = request.form.get("newpassword")
-  if newusername is not None:
-    if len(newusername) > 2 and len(newpassword) > 2:
-      message="Wecome to the site, glad you were able to create an account!"
+  if newusername != '' and newpassword != '':
+    message="Wecome to the site, glad you were able to create an account!"
+    session['logged_in'] = True
+    session['username'] = newusername
+    db.execute("INSERT INTO login (username, password) VALUES (:username, :password)",
+      {"username": newusername, "password": newpassword})
+  elif username != '' and password != '':
+    # See if user exists.
+    rowcount = db.execute("SELECT * FROM login WHERE username = :username and password = :password", {"username": username, "password": password}).rowcount
+    if rowcount == 1:
+      message="Welcome Back!"
+      session['username'] = username
       session['logged_in'] = True
-      db.execute("INSERT INTO login (username, password) VALUES (:username, :password)",
-            {"username": newusername, "password": newpassword})
-    elif len(username) > 2 and len(password) > 2:
-      # See if user exists.
-      rowcount = db.execute("SELECT * FROM login WHERE username = :username and password = :password", {"username": username, "password": password}).rowcount
-      if rowcount == 1:
-        message="Welcome Back!"
-        session['logged_in'] = True
-      elif rowcount == 0:
-        message="No user found, please try logging in again or create an account"
-        session['logged_in'] = False
-  else:
-    message=""
+    elif rowcount == 0:
+      message="No user found, please try logging in again or create an account"
+      session['logged_in'] = False
+
+  #message=""
+
   # All done commit to database!
   db.commit()
   return(render_template("login.html", message=message))
@@ -64,17 +66,19 @@ def location():
     query = requests.get("https://api.darksky.net/forecast/34b6298c5f0fe67d3cc741bb29bb0e22/42.37,-71.11").json()
     #query = requests.get("https://api.darksky.net/forecast/34b6298c5f0fe67d3cc741bb29bb0e22/{lat},{long}").json()
     temp = query["currently"]["temperature"]
+    location_comments = db.execute("SELECT * FROM checkin where zipcode = :zipcode", {"zipcode": zipcode}).fetchall()
     print(f"temp is {temp}")
-    return(render_template("location.html", zipcode=zipcode, returned_zip_info=returned_zip_info, lat=lat, long=long, temp=temp ))
+    return(render_template("location.html", zipcode=zipcode, returned_zip_info=returned_zip_info, lat=lat, long=long, temp=temp, location_comments=location_comments ))
 
 @app.route("/checkin", methods=['POST'])
 def checkin():
+     print('got in here')
      yourusername = request.form.get("yourusername")
      comment = request.form.get("comment")
      zipcode = request.form.get("zipcode")
-     if yourusername is not None:
-       db.execute("INSERT INTO checkin (zipcode, username, comment) VALUES (:zipcode, :username, :comment)",
-            {"zipcode": newusername, "username": yourusername, "comment": comment})
+     print(comment)
+     db.execute("INSERT INTO checkin (zipcode, username, comment) VALUES (:zipcode, :username, :comment)", {"zipcode": zipcode, "username": yourusername, "comment": comment})
+     db.commit()
      return(render_template("location.html"))
 
 @app.route("/currenttemp")
@@ -95,6 +99,7 @@ def searchresults():
     lat = returned_zip_info[4]
     long = returned_zip_info[5]
     query = requests.get("https://api.darksky.net/forecast/34b6298c5f0fe67d3cc741bb29bb0e22/{{lat}},{{long}}").json()
+    print(query)
     #print(json.dumps(query["currently"], indent = 2))
     #temp = query["currently"]["temperature"]
     temp = "84";
